b755a614bab2939117e3fecff0366209a2216f6b
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2023-10-06 18:54:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.36 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,9 @@
-import collections
-
 def solution(participant, completion):
-    answer = collections.Counter(participant) - collections.Counter(completion)
-    return list(answer.keys())[0]
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    
+    return participant[-1]
